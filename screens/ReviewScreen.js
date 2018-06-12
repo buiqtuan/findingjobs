b5329534654a166e2c11d6193bd6e03c2c5c635b
buiqtuan <tuanbq2909@gmail.com>
2018-06-13 00:13:58 +0700

    done sliding components

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Platform} from 'react-native';
+import {Button} from 'react-native-elements';
 
 export default class ReviewScreen extends React.Component {
     static navigationOptions = {
         title: 'Review Jobs',
-        header: () => {
+        header: ({ navigate }) => {
             return {
-                right: <Text>Right</Text>
+                right: (<Button 
+                            title="Settings" 
+                            onPress={() => navigate('setting')}
+                            backgroundColor="rgba(0,0,0,0)"
+                            color="rgba(0,122,255,1)"
+                        />),
+                style: {
+                    marginTop: Platform.OS === 'android' ? 24 : 0
+                }
             };
         }
     }
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
+import Slides from '../components/Slides'
+
+const SLIDES_DATA = [
+    {text: 'Welcome to JobApp', color: '#03A9F4'},
+    {text: 'Use this to get a job', color: '#009688'},
+    {text: 'Set your location, then swipe away', color: '#03A9F4'}
+]
+
 export default class WelcomeScreen extends React.Component {
+    onSlidesComplete = () => {
+        this.props.navigation.navigate('auth');
+    }
+
     render() {
         return(
             <View>
-                <Text> WelcomeScreen </Text>
+                <Slides data={SLIDES_DATA} onComplete={this.onSlidesComplete}/>
             </View>
         );
     }
